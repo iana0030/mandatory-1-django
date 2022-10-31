bd502c630aeb2088ef5e6d8df355141ce2d695ca
DavidHaring20 <davidharingri@gmail.com>
2022-10-31 13:37:42 +0100

    Added take_loan method.

--- project/banking_system/models.py
+++ project/banking_system/models.py
@@ -3,9 +3,6 @@ from django.db import models, transaction
 from django.db.models import Sum
 import random
 
-# BANK_FK is Bank's foreign key used to associate loan accounts with bank and make loan payments in Ledger
-BANK_FK = 'bank_account_fk'
-
 # after every defined method, that method with the same name is added to Django's User class via User.add_to_class method
 
 # represents the User class which is used by bank employees for administration or by regular customers
@@ -106,7 +103,7 @@ class Customer(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f"\n{self.id} - {self.username} - {self.password} - {self.first_name} {self.last_name} - {self.address} - {self.phone_number} - {self.rank} - {self.user}"
+        return f"{self.id} - {self.username} - {self.password} - {self.first_name} {self.last_name} - {self.address} - {self.phone_number} - {self.rank} - {self.user}"
 
     # retrieves all customer's accounts and accounts' movements  
     def get_customer_movements(foreign_key):
@@ -120,14 +117,19 @@ class Customer(models.Model):
         return records
         
     # creates the loan account 
-    def take_loan(self, amount, text):
-        if not self.rank == 'silver' or self.rank == 'gold':
-            return "Can't make a loan. User is of basic rank."
+    def take_loan(self, amount, text, deposit_account_primary_key):
+        if self.rank == 'basic':
+            return "Can't make a loan. User is not of silver or gold rank."
         elif Customer.get_customer_movements == 0:
-            return "Can't make a loan. User has no accounts to deposit money."
+            return "Can't make a loan. User has no accounts to deposit money to."
         else:
-            bank_account = Account.create_loan_account(Account, BANK_FK) 
-            Ledger.make_transactions(bank_account, self, amount, text)
+            # find deposit account, create loan/withdrawal account then transfer money from one to another
+            # in case of error, transaction will ensure ACID
+            deposit_account = Account.objects.get(pk=deposit_account_primary_key)
+            with transaction.atomic():
+                loan_account_account_number = Customer.create_loan_account(self).account_number
+                loan_account = Account.objects.get(account_number=loan_account_account_number)
+                Ledger.make_transactions(loan_account, deposit_account, amount, text)
     
     # creates the transaction to loan account
     def pay_loan(self, amount, text):
@@ -138,9 +140,10 @@ class Customer(models.Model):
         else:
             Ledger.make_transactions(self, BANK_FK, amount, text)
 
-    def create_loan_account(bank_account_fk):
-        random_account_number = random()*10
-        Account.create_account(Account, True, bank_account_fk)
+    def create_loan_account(customer_primary_key):
+        random_account_number = random.getrandbits(64)
+        new_loan_account = Account.create(random_account_number, True, customer_primary_key)
+        return new_loan_account
 
 # represents Account class which can belong to one Customer
 class Account(models.Model):
@@ -191,10 +194,10 @@ class Ledger(models.Model):
 
     def make_transactions(sender_account, receiver_account, amount, text):
         with transaction.atomic():
-            if sender_account.balance >= amount:
+            if sender_account.balance >= amount or sender_account.is_loan == True:
                 Ledger.objects.bulk_create([
-                    Ledger(account_id=sender_account.account_id, amount=-amount, text=text),
-                    Ledger(account_id=receiver_account.account_id, amount=amount, text=text)
+                    Ledger(account=sender_account, amount=-amount, text=text),
+                    Ledger(account=receiver_account, amount=amount, text=text)
                     # Ledger(account=sender_account.account_number, amount=-amount, text=text),
                     # Ledger(account=receiver_account.account_number, amount=amount, text=text)
                 ])
