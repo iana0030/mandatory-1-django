25b271056fd9d1d2c635e1e66be1b411bdbae4e8
DavidHaring20 <davidharingri@gmail.com>
2022-11-07 17:07:21 +0100

    Added create_user and create_customer to views.py, urls.py and index.html.

--- project/banking_system/urls.py
+++ project/banking_system/urls.py
@@ -5,4 +5,6 @@ app_name = "banking_system"
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('create_user', views.create_user, name='create_user'),
+    path('create_customer', views.create_customer, name='create_customer'),
 ]
--- project/banking_system/views.py
+++ project/banking_system/views.py
@@ -1,5 +1,31 @@
 from django.shortcuts import render
+from .models import User, Customer, Account, Ledger
 
 
 def index(request):
     return render(request, 'banking_system/index.html', {})
+
+# USER methods
+def create_user(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+
+        new_user = User.create_user(username, email, first_name, last_name)
+        return render(request, 'banking_system/index.html', {'new_user': new_user})
+
+def create_customer(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        address = request.POST['address']
+        phone_number = request.POST['phone_number']
+        rank = request.POST['rank']
+        user_foreign_key = request.POST['user_foreign_key']
+
+        new_customer = User.create_customer(username, password, first_name, last_name, address, phone_number, rank, user_foreign_key)
+        return render(request, 'banking_system/index.html', {'new_customer': new_customer})
